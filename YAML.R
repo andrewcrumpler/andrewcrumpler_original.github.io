1199e771b27b581352ff2a0d2c2572cab9e70703
andrewcrumpler <crumplerat@appstate.edu>
2018-09-17 01:27:44 -0400

    Finished site

---
+++ YAML.R
@@ -0,0 +1,13 @@
+name: "andrewcrumpler.github.io"
+output_dir:"."
+navbar:
+  title: "Andrew Crumpler"
+  left:
+    - text: "Home"
+      href: index.html
+    - text: "About"
+      href: about.html
+    - text: "Resume"
+      href: resume.html
+    - text: "Cool Links"
+      href: links.html
